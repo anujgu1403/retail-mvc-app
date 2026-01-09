0acbeb966c36f78e89d04f2b2bb223a3ec37ef7b
Anuj Kumar <anujgupta1403@gmail.com>
2026-01-09 10:42:19 +0530

    order view controller

--- src/main/java/com/retail/mvc/controller/OrderViewController.java
+++ src/main/java/com/retail/mvc/controller/OrderViewController.java
@@ -1,12 +1,24 @@
 package com.retail.mvc.controller;
 
 import com.retail.mvc.client.OrderClient;
+import com.retail.mvc.model.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OrderViewController {
 
     @Autowired
     private OrderClient orderClient;
+
+    @PostMapping("/submitcheckout")
+    public String placeOrder(@ModelAttribute Cart cart, Model model) {
+        orderClient.submitCheckout(cart);
+        model.addAttribute("cart", cart);
+        return "cart";
+    }
+
 }
